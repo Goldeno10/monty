73e342ca89a05c644cb86de814e7abe4a7f0f696
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-13 14:13:07 +0000

    ...

--- _error.c
+++ _error.c
@@ -1,9 +1,19 @@
 #include "monty.h"
 
-void _error(int n)
+void _error(int n, )
 {
-}.
-.
+	struc err {
+		int n;
+		char *str;
+		};
+	err err_code[] = {
+		{1, "USAGE: monty file"},
+		{2, "Error: Can't open file"},
+		{3, "Error: malloc failed"}
+	};
+
+}
+
 
 
 
