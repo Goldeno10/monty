c5557266065843d1464883d6b19f8526a1b30a1f
Goldeno10 <ibrahimmuhammad271@gmail.com>
2022-07-14 09:18:17 +0000

    Added tokenizer

--- get_func.c
+++ get_func.c
@@ -1,9 +1,17 @@
 #include "monty.h"
 
-void (*get_opcode(char *format))(stack_t **stack, unsigned int line_number)
+void (*get_opcode(char *opcode))(stack_t **stack, unsigned int line_number)
 {
+	int i;
 	instructions_t ops[] ={
 		{"push", _push},
 		{"pall", _pall},
 		{NULL, NULL}
 	};
+	while (ops[i])
+	{
+		if (strcmp(opcode, ops[i].opcode) == 0)
+			return (ops[i].f)
+		i++;
+	}
+}
